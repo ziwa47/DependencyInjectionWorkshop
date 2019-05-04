aaaa0a556e6b55978a608b2bc77cd86b609e4815
阿輝 杜 <s3004856@hotmail.com>
2019-05-04 16:18:21 +0800

    Notify Log

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -1,9 +1,7 @@
 ﻿using DependencyInjectionWorkshop.Adapter;
 using DependencyInjectionWorkshop.Models;
 using DependencyInjectionWorkshop.Repository;
-
 using NSubstitute;
-
 using NUnit.Framework;
 
 namespace DependencyInjectionWorkshopTests
@@ -15,6 +13,8 @@ namespace DependencyInjectionWorkshopTests
         private const string DefaultHashedPassword = "my hashed password";
         private const string DefaultOtp = "12345";
         private const string DefaultPassword = "pw";
+        private const int DefaultFailedCount = 91;
+
         private IProfile _profile;
         private IOtp _otp;
         private IHash _hash;
@@ -22,7 +22,7 @@ namespace DependencyInjectionWorkshopTests
         private ILogger _logger;
         private IFailedCounter _failedCounter;
         private AuthenticationService _authenticationService;
-        
+
         [SetUp]
         public void SetUp()
         {
@@ -32,23 +32,84 @@ namespace DependencyInjectionWorkshopTests
             _notification = Substitute.For<INotification>();
             _logger = Substitute.For<ILogger>();
             _failedCounter = Substitute.For<IFailedCounter>();
-            _authenticationService = new AuthenticationService(_failedCounter, _profile, _hash, _otp, _logger, _notification);
+            _authenticationService =
+                new AuthenticationService(_failedCounter, _profile, _hash, _otp, _logger, _notification);
+        }
+
+        [Test]
+        public void Is_inValid_When_wrong_opt()
+        {
+            var isValid = WhenInvalid();
+            ShouldBeInvalid(isValid);
         }
 
         [Test]
         public void Is_Valid()
         {
+            var isValid = WhenValid();
+            ShouldBeValid(isValid);
+        }
+
+        [Test]
+        public void Log_account_failed_count_when_invalid()
+        {
+            GivenFailedCount(DefaultFailedCount);
+
+            WhenInvalid();
+
+            LogShouldContains(DefaultAccountId, DefaultFailedCount);
+        }
+
+        [Test]
+        public void notify_user_when_invalid()
+        {
+            WhenInvalid();
+            ShouldNotifyUser();
+        }
+
+        private void GivenFailedCount(int defaultFailedCount)
+        {
+            _failedCounter.Get(DefaultAccountId).ReturnsForAnyArgs(defaultFailedCount);
+        }
+
+        private void LogShouldContains(string defaultAccountId, int defaultFailedCount)
+        {
+            _logger.Received(1).Info(Arg.Is<string>(msg =>
+                msg.Contains(defaultAccountId) && msg.Contains(defaultFailedCount.ToString())));
+        }
+
+        private bool WhenValid()
+        {
+            GivenPassword(DefaultAccountId, DefaultHashedPassword);
+            GivenHash(DefaultPassword, DefaultHashedPassword);
+            GivenOtp(DefaultAccountId, DefaultOtp);
+            var isValid = WhenVerify(DefaultAccountId, DefaultPassword, DefaultOtp);
+            return isValid;
+        }
+
+        private bool WhenInvalid()
+        {
             GivenPassword(DefaultAccountId, DefaultHashedPassword);
             GivenHash(DefaultPassword, DefaultHashedPassword);
             GivenOtp(DefaultAccountId, DefaultOtp);
 
-            var isValid = WhenVerify(DefaultAccountId, DefaultPassword, DefaultOtp);
-            ShouldBeValid(isValid);
+            var isValid = WhenVerify(DefaultAccountId, DefaultPassword, "wrong otp");
+            return isValid;
+        }
+
+        private void ShouldNotifyUser()
+        {
+            _notification.Received(1).PushMessage(Arg.Any<string>());
         }
 
         private static void ShouldBeValid(bool isValid)
         {
             Assert.IsTrue(isValid);
+        }
+
+        private static void ShouldBeInvalid(bool isValid)
+        {
+            Assert.IsFalse(isValid);
         }
 
         private bool WhenVerify(string accountId, string password, string otp)

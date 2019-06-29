ca7e75a88afd027c18ae5de69ea0330bc7109271
阿輝 杜 <s3004856@hotmail.com>
2019-06-29 14:58:57 +0800

    refactor

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -1,5 +1,7 @@
 ﻿using DependencyInjectionWorkshop.Models;
+
 using NSubstitute;
+
 using NUnit.Framework;
 
 namespace DependencyInjectionWorkshopTests
@@ -7,25 +9,63 @@ namespace DependencyInjectionWorkshopTests
     [TestFixture]
     public class AuthenticationServiceTests
     {
+        private readonly INotification _notification = Substitute.For<INotification>();
+        private readonly IProfileDao _profileDao = Substitute.For<IProfileDao>();
+        private readonly IHash _hash = Substitute.For<IHash>();
+        private readonly IOtpService _otpService = Substitute.For<IOtpService>();
+        private readonly IFailedCounter _failedCounter = Substitute.For<IFailedCounter>();
+        private readonly ILogger _logger = Substitute.For<ILogger>();
+        private readonly AuthenticationService _sut;
+        private string DefaultAccount = "joey";
+        private string DefaultPassword = "abc";
+        private string DefaultHashedPassword = "9487";
+        private string DefaultOtp = "9527";
+
+        public AuthenticationServiceTests()
+        {
+            _sut = new AuthenticationService(_profileDao, _hash, _otpService, _failedCounter, _notification, _logger);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
         [Test]
         public void is_valid()
         {
-            var profileDao = Substitute.For<IProfileDao>();
-            var hash = Substitute.For<IHash>();
-            var otpService = Substitute.For<IOtpService>();
-            var failedCounter = Substitute.For<IFailedCounter>();
-            var notification = Substitute.For<INotification>();
-            var logger = Substitute.For<ILogger>();
+            GivenPasswordFromDb(DefaultAccount, DefaultPassword);
+            GivenHashedPassword(DefaultPassword, DefaultHashedPassword);
+            GivenOtp(DefaultOtp, DefaultAccount);
 
-            var authenticationService = new AuthenticationService(profileDao,hash,otpService,failedCounter,notification,logger);
-
-            profileDao.GetPassword("joey").ReturnsForAnyArgs("abc");
-            hash.Hash("9487").ReturnsForAnyArgs("abc");
-            otpService.GetOtpResp("joey").ReturnsForAnyArgs("9527");
-
-            var isValid = authenticationService.Verify("joey", "9487", "9527");
-            Assert.IsTrue(isValid);
+            var isValid = WhenVerify(DefaultAccount, DefaultHashedPassword, DefaultOtp);
+            ShouldValid(isValid);
         }
 
+        private static void ShouldValid(bool isValid)
+        {
+            Assert.True(isValid);
+        }
+
+        private bool WhenVerify(string account, string password, string otp)
+        {
+            var isValid = _sut.Verify(account, password, otp);
+            return isValid;
+        }
+
+        private void GivenOtp(string otp, string account)
+        {
+            _otpService.GetOtpResp(account).ReturnsForAnyArgs(otp);
+        }
+
+        private void GivenHashedPassword(string hashedPassword, string plainText)
+        {
+            _hash.Hash(plainText).ReturnsForAnyArgs(hashedPassword);
+        }
+
+        private void GivenPasswordFromDb(string account, string passwordFromDb)
+        {
+            _profileDao.GetPassword(account).ReturnsForAnyArgs(passwordFromDb);
+        }
     }
 }
